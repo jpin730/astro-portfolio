51e8f46b71b380208c570514908ad08fad68e31b
Jaime Pineda <jpin730@pm.me>
2024-04-26 12:21:50 -0600

    chore: :wrench: add tailwind config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addBase, addComponents, theme }) {
+      addBase({
+        h1: { fontSize: theme('fontSize.4xl') },
+        h2: { fontSize: theme('fontSize.2xl') },
+        h3: { fontSize: theme('fontSize.xl') },
+        h4: { fontSize: theme('fontSize.lg') },
+        h5: { fontSize: theme('fontSize.base') },
+        h6: { fontSize: theme('fontSize.sm') },
+        p: { fontSize: theme('fontSize.base') },
+      })
+      addComponents({
+        '.h': {
+          color: theme('colors.gray.900'),
+          fontWeight: theme('fontWeight.medium'),
+        },
+        '.dark .h': {
+          color: theme('colors.white'),
+        },
+        '.p': { color: theme('colors.gray.500') },
+        '.dark .p': { color: theme('colors.gray.400') },
+        '.bg-light': { backgroundColor: theme('colors.white') },
+        '.bg-dark': { backgroundColor: theme('colors.gray.800') },
+      })
+    },
+  ],
+}
